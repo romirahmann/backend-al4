6bf82e85869b0e8f3ffdc53510f7086c0f72a0b0
romirahman <romirahman03romi@gmail.com>
2024-02-19 19:14:35 +0700

    fix: update

--- src/model/quiz.model.js
+++ src/model/quiz.model.js
@@ -7,14 +7,15 @@ const getAllQuestion = async () =>
       "q.question_id",
       "q.question_text",
       "q.id_area",
+      "q.is_deleted",
       "ar.nama_area",
       "an.answer_id",
-      "an.question_id",
       "an.status"
     )
     .from("questions as q")
     .join("area as ar", "ar.id_area", "q.id_area")
-    .leftJoin("answer as an", "an.question_id", "q.question_id");
+    .leftJoin("answer as an", "an.question_id", "q.question_id")
+    .where("q.is_deleted", 0);
 
 const getAllByAreaId = async (areaID) =>
   await project
@@ -22,9 +23,9 @@ const getAllByAreaId = async (areaID) =>
       "q.question_id",
       "q.question_text",
       "q.id_area",
+      "q.is_deleted",
       "ar.nama_area",
       "an.answer_id",
-      "an.question_id",
       "an.status"
     )
     .from("questions as q")
@@ -34,7 +35,7 @@ const getAllByAreaId = async (areaID) =>
 
 const addNewQuestion = async (data) => await project("questions").insert(data);
 const updateQuestion = async (id, data) =>
-  await project("questions").where("question_id", id).insert(data);
+  await project("questions").where("question_id", id).update(data);
 
 // ANSWER
 const getAnswerByQuestionId = async (id) =>
